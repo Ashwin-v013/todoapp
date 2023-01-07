b434e306e84c3f080456ba85e4575b47b39ccece
Ashwin-v013 <ashwinv013@gmail.com>
2023-01-08 01:21:34 +0530

    fixes

--- components/todos.js
+++ components/todos.js
@@ -3,92 +3,85 @@ import Filter from "./Filter";
 import Todoslist from "./todoslist";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useSelector , useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { statusActions } from "../store/store";
 import Notification from "./notification";
 import { fetchtodo } from "../store/store";
 import { useRouter } from "next/router";
 import Router from "next/router";
 
-
 // let isinitial = true;
 
 const AllTodos = () => {
+  const router = useRouter();
+  const [Route, setRoute] = useState(false);
 
-    const router =useRouter();
-    const [Route , setRoute] = useState(false)
-   
-    const currentfilter = useSelector( state => state.filter);
-    const notification = useSelector(state => state.notification)
-    const todos = useSelector(state => state.todo)
+  const currentfilter = useSelector((state) => state.filter);
+  const notification = useSelector((state) => state.notification);
+  const todos = useSelector((state) => state.todo);
 
-     const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-   
+  useEffect(() => {
+    dispatch(fetchtodo());
+  }, []);
 
-    useEffect(() => {
-    dispatch(fetchtodo()) 
-
-  }, [])
-  
   // if(notification.status === 'success'){
 
-      const DeleteHandler = (id) => {
-      
-    fetch(`https://todoapp-d91e4-default-rtdb.firebaseio.com/Todos/${id}.json`,{
-        method:'delete',
-    }).then(() =>{
-       const updatedTodos =   todos.filter((todo) => todo.id !== id);
-      dispatch(statusActions.notification({
-          status : "deleted",
-          error : "Todo Deleted",
-        }));
-       dispatch(statusActions.addtodo(updatedTodos));     
-    })
-  
- }
-  
-    let jsx = (
-        <Todoslist todos={todos} ondelete={DeleteHandler} />
+  const DeleteHandler = (id) => {
+    fetch(
+      `https://todoapp-d91e4-default-rtdb.firebaseio.com/Todos/${id}.json`,
+      {
+        method: "delete",
+      }
+    ).then(() => {
+      const updatedTodos = todos.filter((todo) => todo.id !== id);
+      dispatch(
+        statusActions.notification({
+          status: "deleted",
+          error: "Todo Deleted",
+        })
+      );
+      dispatch(statusActions.addtodo(updatedTodos));
+    });
+  };
 
-    )
+  let jsx = <Todoslist todos={todos} ondelete={DeleteHandler} />;
 
-    if(currentfilter === 'All'  && todos.length > 0  ) {
+  if (currentfilter === "All" && todos.length > 0) {
+    jsx = <Todoslist todos={todos} ondelete={DeleteHandler} />;
+  }
+  if (currentfilter === "Completed" && todos.length > 0) {
+    jsx = (
+      <Todoslist
+        todos={todos.filter((todo) => todo.status == true)}
+        ondelete={DeleteHandler}
+      />
+    );
+  }
+  if (currentfilter === "Active" && todos.length > 0) {
+    jsx = (
+      <Todoslist
+        todos={todos.filter((todo) => todo.status == false)}
+        ondelete={DeleteHandler}
+      />
+    );
+  }
+  if (todos.length === 0) {
+    jsx = "no post";
+  }
 
-        jsx = <Todoslist todos={todos} ondelete={DeleteHandler} />
-       
-     }
-    if(currentfilter === 'Completed'  && todos.length > 0  ) {
-
-        jsx = <Todoslist todos={todos.filter(todo => todo.status == true)} ondelete={DeleteHandler} />
-       
-     }
-    if(currentfilter === 'Active'  && todos.length > 0  ) {
-
-        jsx = <Todoslist todos={todos.filter(todo => todo.status == false)} ondelete={DeleteHandler} />
-       
-     }
-     if(todos.length === 0) {
-        jsx = 'no post'
-    }
-
-    
-    
-       
   return (
+    <>
+      {/* { notification && <Notification title={notification.status} message={notification.error}/> } */}
+      {/* {notifyjsx} */}
+      <Filter />
 
-<> 
- {/* { notification && <Notification title={notification.status} message={notification.error}/> } */}
-{/* {notifyjsx} */}
-<Filter  />
+      <p className="Status">{currentfilter} todos are listed below:</p>
 
-   <p className="Status">{currentfilter} todos are listed below:</p>
-  
-    <ul>
-      {jsx}
-    </ul>
+      <ul>{jsx}</ul>
     </>
-  )
-}
+  );
+};
 
 export default AllTodos;
